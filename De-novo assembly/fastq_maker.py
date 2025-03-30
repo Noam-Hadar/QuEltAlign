5696a163433d8787e5d5440cdbeea04e77d91e65
NoamHadar <noamhadars@gmail.com>
2025-03-30 23:44:20 +0300

    fastq_maker update

--- De-novo assembly/fastq_maker.py
+++ De-novo assembly/fastq_maker.py
@@ -9,18 +9,24 @@ for fasta in fastas:
     print(gene)
     os.system(cmd)
     os.rename('output.aln', gene + '.aln')
-    with open('output.fq', 'r') as f:
-        n = 1
-        for line in f:
-            if n == 2:
-                open(gene + '.sequences.txt', 'a').write(line)
-                l = len(line.strip())
-            if n == 4:
-                line = 'F'*l + '\n'
-            if n == 4:
-                n = 0
-            n += 1
-            open(gene + '.fastq', 'a').write(line)
+    
+    aln_file = [i for i in open(f'{gene}.aln', 'r').read().split('\n')[4:] if i != '']
+    read_ids = aln_file[::3]
+    real_sequences = aln_file[1::3]
+    simulated_sequences = aln_file[2::3]
+    fastq_reads = []
+    reads = []
+    for n, read_id in enumerate(read_ids):
+        if real_sequences[n] == simulated_sequences[n]:
+            read_coordinate = read_id.split('\t')[1].strip()
+            read_sequence = real_sequences[n].strip()
+            phred_score = 'F'*len(read_sequence.strip())
+            read = f'@{read_coordinate}\n{read_sequence}\n+\n{phred_score}'
+            reads.append(read_sequence)
+            fastq_reads.append(read)
+    
+    open(gene + '.sequences.txt', 'a').write('\n'.join(reads))
+    open(gene + '.fastq', 'a').write('\n'.join(fastq_reads))
     os.remove('output.fq')
     shutil.move(gene + '.sequences.txt', path + '/' + gene + '.sequences.txt')
     for suffix in ('.aln', '.fastq'):
